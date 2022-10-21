59ec075a2b93c41b6b3c52d741a42e67054b0763
Melissa Bracamonte <melissabracamonte30@gmail.com>
2022-10-21 18:55:07 -0500

    maquetado y diseño del header

---
+++ src/components/report/ReportDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../report/header/Header";
+import ResultsDashboard from './resultsDashboard/ResultsDashboard';
+
+const ReportDashboard = () => {
+  return (
+    <>
+    <Header/>
+    <ResultsDashboard/>
+    </>
+  )
+}
+
+export default ReportDashboard
---
+++ src/components/report/resultsDashboard/ResultsDashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ResultsDashboard = () => {
+  return (
+    <>
+      <main>
+        <h4>Análisis de sésgos de género</h4>
+      </main>
+    </>
+  );
+};
+
+export default ResultsDashboard;
